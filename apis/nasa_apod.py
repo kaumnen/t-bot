2f48f727c7b84f1de8706db70f3476d065ecde54
Aleksej Komnenovic <main@komnen.xyz>
2020-10-12 14:37:19 +0200

    heroku final, working for now

--- apis/nasa_apod.py
+++ apis/nasa_apod.py
@@ -22,7 +22,6 @@ class Nasa_apod:
         self.apod = nasa.picture_of_the_day(date=d, hd=True)
 
     def retrieve_picture_url(self):
-        print(self.apod)
         return self.apod["url"]
 
     def retrieve_picture_info(self):
